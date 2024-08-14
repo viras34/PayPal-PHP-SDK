a794b3aea9a7059d6044fcc875e632faf05be162
viras34 <s.dadu@outlook.com>
2024-08-14 09:17:40 +0200

    Update ReflectionUtil.php

--- lib/PayPal/Common/ReflectionUtil.php
+++ lib/PayPal/Common/ReflectionUtil.php
@@ -148,8 +148,16 @@ class ReflectionUtil
      */
     public static function getter($class, $propertyName)
     {
-        return method_exists($class, "get" . ucfirst($propertyName)) ?
-            "get" . ucfirst($propertyName) :
-            "get" . preg_replace_callback("/([_\-\s]?([a-z0-9]+))/", "self::replace_callback", $propertyName);
+        if (method_exists($class, "get" . ucfirst($propertyName))) {
+            return "get" . ucfirst($propertyName);
+        }
+
+        return "get" . preg_replace_callback(
+                "/([_\-\s]?([a-z0-9]+))/",
+                function($matches) {
+                    return ucfirst($matches[2]);
+                },
+                $propertyName
+            );
     }
 }
